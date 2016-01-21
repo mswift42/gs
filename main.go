5de953aac88c37314888c03358f22de542c3030a
mswift42 <martin.haesler@gmail.com>
2016-01-21 18:33:09 +0000

    Rename avatarurl to avatarURL

--- main.go
+++ main.go
@@ -9,10 +9,10 @@ import (
 
 func main() {
 	client := github.NewClient(nil)
-	fmt.Println(avatarUrl("mswift42", client))
+	fmt.Println(avatarURL("mswift42", client))
 }
 
-func avatarUrl(usr string, client *github.Client) string {
+func avatarURL(usr string, client *github.Client) string {
 	user, _, err := client.Users.Get(usr)
 	if err != nil {
 		log.Fatal(err)
