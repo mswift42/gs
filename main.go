3dc45067c19381eb1de74a772f895d22845c960a
mswift42 <martin.haesler@gmail.com>
2016-01-22 12:49:08 +0000

    Add generic searchTemplate

--- main.go
+++ main.go
@@ -5,11 +5,29 @@ import (
 	"log"
 
 	"github.com/google/go-github/github"
+	"launchpad.net/go-unityscopes/v2"
 )
+
+const searchCategoryTemplate = `
+  "schema-version": 1,
+  "template": {
+    "category-layout": "grid",
+    "card-size": "small"
+  },
+  "components": {
+    "title": "title",
+    "art":  "art",
+    "subtitle": "username"
+  }
+}`
 
 func main() {
 	client := github.NewClient(nil)
 	fmt.Println(avatarURL("mswift42", client))
+}
+
+type GithubScope struct {
+	base *scopes.ScopeBase
 }
 
 func avatarURL(usr string, client *github.Client) string {
